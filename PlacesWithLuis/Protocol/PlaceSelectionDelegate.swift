f21d96583e7aca20ca800930ddc5b8116c6f16f5
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-06 15:15:37 -0500

    Project finished

---
+++ PlacesWithLuis/Protocol/PlaceSelectionDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  PlaceSelectionDelegate.swift
+//  PlacesWithLuis
+//
+//  Created by Ilgar Ilyasov on 9/6/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+protocol PlaceSelectionDelegate: class {
+    func placeWasSelected(place: Place)
+}
--- PlacesWithLuis/View Controller/VisitedPlacesViewController.swift
+++ PlacesWithLuis/View Controller/VisitedPlacesViewController.swift
@@ -8,30 +8,36 @@
 
 import UIKit
 
-class VisitedPlacesViewController: UIViewController, PlacesPresenter {
+class VisitedPlacesViewController: UIViewController, PlacesPresenter, PlaceSelectionDelegate {
 
     var placeController: PlaceContoller?
+    var placeTableViewController: PlacesTableViewController?
+    var mapViewController: MapViewController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
+    func placeWasSelected(place: Place) {
+        mapViewController?.location = place.location
+        
+    }
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "MapViewSegue" {
+            
+            guard let mapVC = segue.destination as? MapViewController else { return }
+            mapViewController = mapVC
+            
+        } else if segue.identifier == "PlacesTableSegue" {
+            
+            guard let placesTVC = segue.destination as? PlacesTableViewController else { return }
+            placesTVC.placeController = placeController
+            placesTVC.delegate = self
+            placeTableViewController = placesTVC
+        }
     }
-    */
-
 }
