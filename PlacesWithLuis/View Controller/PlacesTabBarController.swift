d590cb768c1083fea10a1c0511c6bc7093465d01
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-06 12:18:09 -0500

    Related subclass files created and assigned to the related storyboard views

---
+++ PlacesWithLuis/View Controller/PlacesTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  PlacesTabBarController.swift
+//  PlacesWithLuis
+//
+//  Created by Ilgar Ilyasov on 9/6/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class PlacesTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
