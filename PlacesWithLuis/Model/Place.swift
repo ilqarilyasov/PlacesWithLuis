29c61f3f27159475c2b666639319beb1c1de1526
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-06 12:09:25 -0500

    Completed model

---
+++ PlacesWithLuis/Model/Place.swift
@@ -0,0 +1,21 @@
+//
+//  Place.swift
+//  PlacesWithLuis
+//
+//  Created by Ilgar Ilyasov on 9/6/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Place {
+    
+    let name: String
+    let location: (latitude: Double, longitude: Double)
+    
+    init(name: String, latitude: Double, longitude: Double) {
+        
+        self.name = name
+        self.location = (latitude: latitude, longitude: longitude)
+    }
+}
